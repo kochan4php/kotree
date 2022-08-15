56a466279f1416f548dc41710799b71516da0788
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-08-16 04:02:45 +0700

    add: gabut

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import Layout from "@/layouts/index";
 
 const App = () => (
   <Layout>
-    <img src="/vite.svg" alt="avatar" width="100px" />
+    <div className="text-white">
+      <div className="flex flex-col items-center gap-8">
+        <img src="/vite.svg" alt="avatar" width="80px" />
+        <h1 className="text-2xl font-medium">Deo Subarno</h1>
+      </div>
+      <div></div>
+    </div>
   </Layout>
 );
 
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,5 +1,5 @@
 const Layout = ({ children }) => (
-  <div className="w-full md:max-w-5xl mx-auto bg-pink-700 min-h-screen flex flex-col items-center justify-center">
+  <div className="w-full md:max-w-5xl mx-auto bg-pink-600 min-h-screen flex flex-col items-center justify-center">
     <main>{children}</main>
   </div>
 );
