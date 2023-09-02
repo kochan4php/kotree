d35cd7babbc74a415ce3f9c443c72785de74ea10
kochan4php <aprodeosubarno@gmail.com>
2023-09-02 10:02:20 +0700

    add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
@@ -8,5 +9,5 @@ export default {
             center: true,
         },
     },
-    plugins: [],
+    plugins: [daisyui],
 };
