251c1bbb2746c1a4cffabef65519c0119464c712
kochan4php <aprodeosubarno@gmail.com>
2023-09-02 10:14:33 +0700

    change button component from arrow function to function declaration

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,22 +5,22 @@
 
 import { ButtonProps } from '../interfaces';
 
-const Button = ({ text, btncolor, icon, ...another }: ButtonProps) => (
-    <button
-        className={`btn ${btncolor} btn-block transition-all duration-300 outline-transparent rounded-md outline-none text-xl capitalize !text-white ${
-            icon ? 'flex gap-2 items-center' : ''
-        }`}
-        {...another}
-    >
-        {icon ? (
-            <>
-                {icon}
-                {text}
-            </>
-        ) : (
-            <>{text}</>
-        )}
-    </button>
-);
-
-export default Button;
+export default function Button({ text, btncolor, icon, ...another }: ButtonProps) {
+    return (
+        <button
+            className={`btn ${btncolor} btn-block transition-all duration-300 outline-transparent rounded-md outline-none text-xl capitalize !text-white ${
+                icon ? 'flex gap-2 items-center' : ''
+            }`}
+            {...another}
+        >
+            {icon ? (
+                <>
+                    {icon}
+                    {text}
+                </>
+            ) : (
+                <>{text}</>
+            )}
+        </button>
+    );
+}
