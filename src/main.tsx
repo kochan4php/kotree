6428731ca58f8ad65d3fd2a1179585dc6c45cd48
kochan4php <aprodeosubarno@gmail.com>
2023-09-02 12:13:00 +0700

    remove react strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(<App />);
