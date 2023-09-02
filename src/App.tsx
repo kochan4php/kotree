d530144461e5e3b23ab4041925db4aba517f6253
kochan4php <aprodeosubarno@gmail.com>
2023-09-02 11:55:15 +0700

    add animation using react-awesome-reveal

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@
  * Don't edit this code!
  */
 
+import { Fade } from 'react-awesome-reveal';
 import Footer from './sections/Footer';
 import Profile from './sections/Profile';
 import SocialMedia from './sections/SocialMedia';
 
 export default function App() {
     return (
-        <div className="w-full sm:max-w-xl mx-auto flex flex-col items-center justify-center px-3 md:px-0">
-            <div className="text-white justify-around min-h-screen flex gap-10 flex-col w-full py-9">
-                <Profile />
-                <SocialMedia />
-                <Footer />
+        <Fade direction="left">
+            <div className="w-full sm:max-w-xl mx-auto flex flex-col items-center justify-center px-3 md:px-0">
+                <div className="text-white justify-around min-h-screen flex gap-10 flex-col w-full py-9">
+                    <Profile />
+                    <SocialMedia />
+                    <Footer />
+                </div>
             </div>
-        </div>
+        </Fade>
     );
 }
