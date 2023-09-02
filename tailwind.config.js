3f55b93c765f6c6f0ee99544f88ea810d40aa967
kochan4php <aprodeosubarno@gmail.com>
2023-09-02 10:00:36 +0700

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+        container: {
+            padding: '1rem',
+            center: true,
+        },
+    },
+    plugins: [],
+};
