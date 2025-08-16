79e804213eb855b53cc3959f3d9c5aff2723887c
kochan4php <aprodeosubarno@gmail.com>
2025-08-16 08:22:15 +0700

    add default value mongodb connection

--- src/connections/mongodb.ts
+++ src/connections/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient(process.env.MONGODB_URL || '');
+const client = new MongoClient(process.env.MONGODB_URL || 'mongodb://localhost:27017');
 const clientPromise = client.connect();
 
 export default clientPromise;
