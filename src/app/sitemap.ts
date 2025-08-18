0c171c5303afc504b13483d85e0656c9cfa6bb73
kochan4php <aprodeosubarno@gmail.com>
2025-08-18 20:14:50 +0700

    update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,7 @@ const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: baseUrl,
+      url: baseUrl + '/',
       lastModified: new Date().toISOString().split('T')[0],
       changeFrequency: 'monthly',
       priority: 1,
